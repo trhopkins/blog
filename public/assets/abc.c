e82f4dcde475110be6df5a4264cb5ba038f4919f
Travis Reid Hopkins <trh.foss@protonmail.com>
2024-11-11 22:26:42 -0500

    totally reset before TF version

--- public/assets/abc.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(int argc, char** argv) {
-    printf("Hello, %s\n", "Reid Hopkins"); // a string
-    printf("The year is %d\n", 2022); // a decimal number
-    printf("Hello, %s\n", "Reid Hopkins", 2022); // a string and an unused number
-    printf("Hello, %p\n", "Reid Hopkins"); // a "pointer" which segfaults immediately
-    printf("Hello, %x\n"); // a hexadecimal number pointing to... random stackspace
-    return 0;
-}
