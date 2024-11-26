8cd20f1306f3f0127c84f67b9ac9010ead889d33
Travis Reid Hopkins <trh.foss@protonmail.com>
2024-11-25 19:26:33 -0500

    feat: added homepage to deployment, except for posts

--- tf/s3.tf
+++ tf/s3.tf
@@ -9,12 +9,66 @@ resource "aws_s3_bucket_versioning" "blog" {
   }
 }
 
-resource "aws_s3_object" "blog_contents" {
-  for_each = fileset("../html/", "*")
+resource "aws_s3_object" "blog_contents_js" {
+  for_each = fileset("../html/public/", "**.js")
   bucket = aws_s3_bucket.blog.id
   key = each.value
-  source = "../html/${each.value}"
-  etag = filemd5("../html/${each.value}")
+  source = "../html/public/${each.value}"
+  etag = filemd5("../html/public/${each.value}")
+  content_type = "application/javascript"
+}
+
+resource "aws_s3_object" "blog_contents_ico" {
+  for_each = fileset("../html/public/", "**.ico")
+  bucket = aws_s3_bucket.blog.id
+  key = each.value
+  source = "../html/public/${each.value}"
+  etag = filemd5("../html/public/${each.value}")
+  content_type = "image/x-icon"
+}
+
+resource "aws_s3_object" "blog_contents_png" {
+  for_each = fileset("../html/public/", "**.png")
+  bucket = aws_s3_bucket.blog.id
+  key = each.value
+  source = "../html/public/${each.value}"
+  etag = filemd5("../html/public/${each.value}")
+  content_type = "image/png"
+}
+
+resource "aws_s3_object" "blog_contents_svg" {
+  for_each = fileset("../html/public/", "**.svg")
+  bucket = aws_s3_bucket.blog.id
+  key = each.value
+  source = "../html/public/${each.value}"
+  etag = filemd5("../html/public/${each.value}")
+  content_type = "image/svg+xml"
+}
+
+resource "aws_s3_object" "blog_contents_xml" {
+  for_each = fileset("../html/public/", "**.xml")
+  bucket = aws_s3_bucket.blog.id
+  key = each.value
+  source = "../html/public/${each.value}"
+  etag = filemd5("../html/public/${each.value}")
+  content_type = "text/xml"
+}
+
+resource "aws_s3_object" "blog_contents_css" {
+  for_each = fileset("../html/public/", "**.css")
+  bucket = aws_s3_bucket.blog.id
+  key = each.value
+  source = "../html/public/${each.value}"
+  etag = filemd5("../html/public/${each.value}")
+  content_type = "text/css"
+}
+
+resource "aws_s3_object" "blog_contents_html" {
+  for_each = fileset("../html/public/", "**.html")
+  bucket = aws_s3_bucket.blog.id
+  key = each.value
+  source = "../html/public/${each.value}"
+  etag = filemd5("../html/public/${each.value}")
   content_type = "text/html"
 }
 
@@ -29,7 +83,7 @@ resource "aws_s3_bucket_website_configuration" "blog_config" {
 }
 
 resource "aws_s3_bucket_public_access_block" "blog_config" {
-  for_each = fileset("../html/", "*")
+  for_each = fileset("../html/public/", "*")
   bucket = aws_s3_bucket.blog.id
   block_public_acls = false
   block_public_policy = false
