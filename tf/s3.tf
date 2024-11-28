de51768abed62687fb0f848f7773b8a5a6420491
Travis Reid Hopkins <trh.foss@protonmail.com>
2024-11-27 21:41:49 -0500

    feat: added CloudFront, work in progress

--- tf/s3.tf
+++ tf/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "blog" {
-	bucket = "camphopkins-xyz"
+  bucket = var.bucket_name
 }
 
 resource "aws_s3_bucket_versioning" "blog" {
@@ -15,29 +15,29 @@ variable "upload_dir" {
 
 variable "mime_types" {
   default = {
-    htm   = "text/html"
-    html  = "text/html"
-    css   = "text/css"
-    xml   = "text/xml"
-    svg   = "image/svg+xml"
-    png   = "image/png"
-    ico   = "image/x-icon"
-    jpeg  = "image/jpeg"
-    svg   = "image/svg"
-    ttf   = "font/ttf"
-    js    = "application/javascript"
-    map   = "application/javascript"
-    json  = "application/json"
+    htm  = "text/html"
+    html = "text/html"
+    css  = "text/css"
+    xml  = "text/xml"
+    svg  = "image/svg+xml"
+    png  = "image/png"
+    ico  = "image/x-icon"
+    jpeg = "image/jpeg"
+    svg  = "image/svg"
+    ttf  = "font/ttf"
+    js   = "application/javascript"
+    map  = "application/javascript"
+    json = "application/json"
   }
 }
 
 resource "aws_s3_object" "blog_contents" {
   for_each = fileset(var.upload_dir, "**/*.*")
-  bucket = aws_s3_bucket.blog.id
-  key = replace(each.value, var.upload_dir, "")
-  source = "${var.upload_dir}/${each.value}"
+  bucket   = aws_s3_bucket.blog.id
+  key      = replace(each.value, var.upload_dir, "")
+  source   = "${var.upload_dir}/${each.value}"
   # acl = "public-read"
-  etag = filemd5("${var.upload_dir}/${each.value}")
+  etag         = filemd5("${var.upload_dir}/${each.value}")
   content_type = lookup(var.mime_types, split(".", each.value)[length(split(".", each.value)) - 1])
 }
 
@@ -47,16 +47,26 @@ resource "aws_s3_bucket_website_configuration" "blog_config" {
     suffix = "index.html"
   }
   error_document {
-    key = "error.html"
+    key = "404.html"
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "blog_config" {
+  bucket = aws_s3_bucket.blog.bucket
+  cors_rule {
+    allowed_headers = ["Authorization", "Content-Length"]
+    allowed_methods = ["GET", "POST"]
+    allowed_origins = ["https://${var.domain_name}"]
+    max_age_seconds = 3000
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "blog_config" {
-  for_each = fileset("../html/public/", "*")
-  bucket = aws_s3_bucket.blog.id
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
+  for_each                = fileset("../html/public/", "*")
+  bucket                  = aws_s3_bucket.blog.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
@@ -68,23 +78,23 @@ resource "aws_s3_bucket_policy" "blog_config" {
 data "aws_iam_policy_document" "allow_public_s3_access" {
   statement {
     principals {
-      type = "AWS"
+      type        = "AWS"
       identifiers = ["*"]
     }
     actions = [
       "s3:GetObject",
     ]
-    resources = [ format("%s/*", aws_s3_bucket.blog.arn) ]
+    resources = [format("%s/*", aws_s3_bucket.blog.arn)]
   }
   statement {
     principals {
-      type = "AWS"
+      type        = "AWS"
       identifiers = ["arn:aws:iam::531782379741:user/tf"]
     }
     actions = [
       "s3:PutObject",
     ]
-    resources = [ format("%s/*", aws_s3_bucket.blog.arn) ]
+    resources = [format("%s/*", aws_s3_bucket.blog.arn)]
   }
 }
 
--- tf/terraform.tf
+++ tf/terraform.tf
@@ -1,21 +1,21 @@
 terraform {
-	required_providers {
-		aws = {
-			source = "hashicorp/aws"
-			version = "~> 5.0"
-		}
-	}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
   backend "s3" {
-    bucket = "camphopkins-xyz-terraform"
-    key = "terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
+    bucket         = "camphopkins-xyz-terraform"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
     dynamodb_table = "camphopkins-xyz-state-lock"
   }
 }
 
 provider "aws" {
-	region = "us-east-1"
+  region     = "us-east-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
