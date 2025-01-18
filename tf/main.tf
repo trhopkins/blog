c482e6609c04e20c17d0edbd4d12ebff515309fa
Travis Reid Hopkins <trh.foss@protonmail.com>
2025-01-18 13:57:11 -0500

    fix: WIP

--- tf/main.tf
+++ tf/main.tf
@@ -1,35 +1,35 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
 
   backend "s3" {
-    bucket         = "camp-hopkins-tf-state"
-    dynamodb_table = "camp-hopkins-tf-lock"
+    bucket         = "camphopkins-tf-state"
+    # dynamodb_table = "camphopkins-tf-lock"
     encrypt        = true
     key            = "terraform.tfstate"
+    region         = "us-east-1"
   }
 }
 
-module "camp-hopkins-static-site" {
-  bucket_name      = "camp-hopkins-content-bucket"
-  cf_dist_name     = "camp-hopkins-cf"
+module "camphopkins-static-site" {
+  bucket_name      = "camphopkins-content-bucket"
+  cf_dist_name     = "camphopkins-cf"
   path_to_bundle   = "../dist"
-  project_name     = "camp-hopkins"
+  project_name     = "camphopkins"
   root_domain_name = "camphopkins.com"
   source           = "./modules/static-site"
 }
 
 module "terraform-be" {
-  secret_key = var.secret_key
-  access_key = var.access_key
-  bucket_name          = "camp-hopkins-tf-state"
-  dynamodb_name        = "camp-hopkins-tf-lock"
-  enable_state_locking = true
-  project_name         = "camp-hopkins"
+  secret_key           = var.secret_key
+  access_key           = var.access_key
+  bucket_name          = "camphopkins-tf-state"
+  dynamodb_name        = "camphopkins-tf-lock"
+  enable_state_locking = false
+  project_name         = "camphopkins"
   source               = "./modules/backend"
 }
-
--- tf/modules/backend/main.tf
+++ tf/modules/backend/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region  = var.region
+  region  = var.aws_region
   access_key = var.access_key
   secret_key = var.secret_key
 }
